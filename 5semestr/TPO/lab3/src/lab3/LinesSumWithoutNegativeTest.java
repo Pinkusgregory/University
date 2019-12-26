9a8716ec086d7c47e61be7de9c1b764b61e189df
dsdcdk <dsdcdk@mail.ru>
2019-12-26 21:25:39 +0300

    Add program to lab3

---
+++ 5semestr/TPO/lab3/src/lab3/LinesSumWithoutNegativeTest.java
@@ -0,0 +1,21 @@
+package lab3;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class LinesSumWithoutNegativeTest {
+	 	public static void TestsComparision(ArrayList<Test> tests) throws IOException {
+	 		FileLogger fileLogger = new FileLogger();
+	 		for (Test t : tests) {
+	 			String result;
+	 			LinesSumWithoutNegative obj = new LinesSumWithoutNegative();
+	 			if (obj.Solve(t.Input) == t.Result) {
+	 				result = t.Name + " is Correct";
+	 			}else {
+					result = t.Name + " is Wrong, expected: " + Integer.toString(t.Result) + " actual: " + Integer.toString(obj.Solve(t.Input));
+				}
+	 			fileLogger.Log(result);
+	 		}
+	 		fileLogger.Close();
+	 	}
+}
---
+++ 5semestr/TPO/lab3/src/lab3/Test.java
@@ -0,0 +1,7 @@
+package lab3;
+
+public class Test {
+	public int Name;
+	public int[][] Input;
+	public int Result;
+}
