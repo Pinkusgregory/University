9b3b40db802226c2599941f487e0dbda9820999f
Nikita <36996976+Pinkusgregory@users.noreply.github.com>
2020-05-12 21:05:02 +0300

    Add lab4 RMP

---
+++ 6semestr/RMP/Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.lab4;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView tvText;
+    SensorManager sensorManager;
+    Sensor sensorAccel;
+    Sensor sensorLinAccel;
+    Sensor sensorGravity;
+
+    StringBuilder sb = new StringBuilder();
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvText = (TextView) findViewById(R.id.tvText);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, sensorAccel,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showInfo();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 400);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+        timer.cancel();
+    }
+
+    String format(float values[]) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1],
+                values[2]);
+    }
+
+    void showInfo() {
+        sb.setLength(0);
+        sb.append("Rotation speed: " + format(valuesAccelMotion));
+        tvText.setText(sb);
+    }
+
+    float[] valuesAccel = new float[3];
+    float[] valuesAccelMotion = new float[3];
+    float[] valuesAccelGravity = new float[3];
+
+    SensorEventListener listener = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i = 0; i < 3; i++) {
+                        valuesAccel[i] = event.values[i];
+                        valuesAccelGravity[i] = (float) (0.1 * event.values[i] + 0.9 * valuesAccelGravity[i]);
+                        valuesAccelMotion[i] = event.values[i]
+                                - valuesAccelGravity[i];
+                    }
+                    break;
+            }
+
+        }
+
+    };
+
+}
